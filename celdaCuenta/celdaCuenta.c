9b29656023b1c79cffb15823e0257b6bbe588338
whtshdowmdq <facucuadradomdq@gmail.com>
2023-11-07 19:15:02 -0300

    Facu; creado libreria celdaCuenta

---
+++ celdaCuenta/celdaCuenta.c
@@ -0,0 +1,5 @@
+//
+// Created by facku on 07/11/2023.
+//
+
+#include "celdaCuenta.h"
---
+++ celdaCuenta/celdaCuenta.h
@@ -0,0 +1,11 @@
+
+#include "../persistencias/cuentas/persistenciaCuentas.h"
+#include "../listaMovimientos/listaMovimientos.h"
+
+typedef struct {
+    stCuenta dato;
+    nodoLista *listaMovimiento;
+} celda;
+
+
+
