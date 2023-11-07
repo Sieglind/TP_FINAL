533d80c7f6df9ed5c5ed874b142ba0c44cf40b86
whtshdowmdq <facucuadradomdq@gmail.com>
2023-11-07 20:50:23 -0300

    Facu; agregada celda a la struct nodoArbol

--- arbolClientes/arbolCliente.h
+++ arbolClientes/arbolCliente.h
@@ -1,10 +1,12 @@
 #include "../persistencias/clientes/persistenciaClientes.h"
+#include "../celdaCuenta/celdaCuenta.h"
 #include <stdlib.h>
 
 typedef struct nodoArbol {
     stCliente cliente;
     struct nodoArbol *izquierda;
     struct nodoArbol *derecha;
+    celda cuentas[];
 } nodoArbol;
 
 nodoArbol *crearNodoArbol(stCliente cliente);
